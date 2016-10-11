6d417134e0ec357d5bf66e4a57eb6924ca062eeb
sycao5 <sycao5@gmail.com>
2016-10-11 18:58:45 -0500

    Add a new query script for rendering upstream graph for multiple runs.

--- make.sh
+++ make.sh
@@ -13,3 +13,7 @@ $QUERIES_DIR/materialize_rm_views.sh > $VIEWS_DIR/rm_views.P
 # draw complete multiple runs graph
 $QUERIES_DIR/render_complete_graph.sh > $RESULTS_DIR/complete_db_graph.gv
 dot -Tpdf $RESULTS_DIR/complete_db_graph.gv > $RESULTS_DIR/complete_db_graph.pdf
+
+# draw worfklow graph upstream of C3_fraction_data
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1.sh output31_hash_value > $RESULTS_DIR/wf_upstream_of_test_data.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_test_data.gv > $RESULTS_DIR/wf_upstream_of_test_data.pdf
