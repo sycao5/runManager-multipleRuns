2b17ac8c553175539ec4986bfeb0c2c135d9384d
sycao5 <sycao5@gmail.com>
2016-10-11 19:01:48 -0500

    Add another test.

--- make.sh
+++ make.sh
@@ -17,3 +17,6 @@ dot -Tpdf $RESULTS_DIR/complete_db_graph.gv > $RESULTS_DIR/complete_db_graph.pdf
 # draw worfklow graph upstream of C3_fraction_data
 $QUERIES_DIR/render_rm_graph_upstream_of_file_q1.sh output31_hash_value > $RESULTS_DIR/wf_upstream_of_test_data.gv
 dot -Tpdf $RESULTS_DIR/wf_upstream_of_test_data.gv > $RESULTS_DIR/wf_upstream_of_test_data.pdf
+
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1.sh output112_hash_value > $RESULTS_DIR/wf_upstream_of_test_data_2.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_test_data_2.gv > $RESULTS_DIR/wf_upstream_of_test_data_2.pdf
