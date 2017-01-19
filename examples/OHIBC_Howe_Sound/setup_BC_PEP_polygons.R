7be05241086af208f8e1249ce366fc36d7cdd15c
sycao5 <sycao5@gmail.com>
2017-01-19 15:28:59 -0600

    Add YW annotation to setup_BC_PEP_polygons.R .

--- examples/OHIBC_Howe_Sound/setup_BC_PEP_polygons.R
+++ examples/OHIBC_Howe_Sound/setup_BC_PEP_polygons.R
@@ -1,8 +1,21 @@
-source("setup.R")
-
 ## Read in BC Parks, Ecological Reserves, and Protected Areas data
-
 # DataBC offers spatial data on BC protected areas with small differences from the WDPA dataset.  We will use this as well.
+
+# @BEGIN Read_BC_Parks_EcologicalReserves_ProtectedArea_data
+# @in poly_hs_pep @AS TA_PEP_SVW_polygon @URI file:_raw_data/databc/TA_PEP_SVW
+# @in setup_configuration_file @AS setup_R
+# @out poly_hs_pep
+
+# @BEGIN set_up_Configuration_and_start_provenance_tracking
+# @in setup_configuration_file @AS setup_R
+# @out dir_anx
+source("setup.R")
+# @END set_up_Configuration_and_start_provenance_tracking
+
+# @BEGIN read_TA_PEP_SVW_polygon
+# @in dir_anx
+# @in poly_hs_pep @AS TA_PEP_SVW_polygon @URI file:{dir_anx}/_raw_data/databc/TA_PEP_SVW
+# @out poly_hs_pep @AS hs_pep_poly
 
 message("reading TA_PEP_SVW_polygon...")
 poly_hs_pep <- readOGR(dsn = file.path(dir_anx, '_raw_data/databc/TA_PEP_SVW'),
@@ -11,6 +24,12 @@ poly_hs_pep <- readOGR(dsn = file.path(dir_anx, '_raw_data/databc/TA_PEP_SVW'),
   spTransform(CRS(p4s_bcalb)) %>%
   crop(ext_howe)
 
+# @END read_TA_PEP_SVW_polygon
+
+# @BEGIN write_hs_pep_poly
+# @in poly_hs_pep @AS hs_pep_poly
+# @out poly_hs_pep
+
 message("Writing outp hs_pep_ploy...")
 writeOGR(poly_hs_pep,
          dsn = path.expand(file.path(dir_goal, 'spatial')),
@@ -18,3 +37,7 @@ writeOGR(poly_hs_pep,
          driver = 'ESRI Shapefile',
          overwrite_layer = TRUE)
 message("completed BC_PEP_polygons.")
+
+# @END write_hs_pep_poly
+
+# @END Read_BC_Parks_EcologicalReserves_ProtectedArea_data
