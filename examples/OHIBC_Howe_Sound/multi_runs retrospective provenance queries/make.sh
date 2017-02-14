592b4cd365b849063d8553906e1d7165eede4719
sycao5 <sycao5@gmail.com>
2017-02-14 16:36:05 -0600

    Improve the makefile script; change the graph layouts to "TB".

--- examples/OHIBC_Howe_Sound/multi_runs retrospective provenance queries/make.sh
+++ examples/OHIBC_Howe_Sound/multi_runs retrospective provenance queries/make.sh
@@ -24,24 +24,33 @@ dot -Tpdf $RESULTS_DIR/complete_db_graph_$joinOn.gv > $RESULTS_DIR/complete_db_g
 dot -Tsvg $RESULTS_DIR/complete_db_graph_$joinOn.gv > $RESULTS_DIR/complete_db_graph_$joinOn.svg
 
 # draw worfklow graph upstream of lsp_trend.csv using sha256 but display filePath in the results
-##productName="lsp_trend"
-##joinOn="filePath"
-##productHashCode=97b9fe2837579bac75e964fe212402ba5ac595bb539bce4f7897be9cdcb55638
-$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_filePath.sh \'97b9fe2837579bac75e964fe212402ba5ac595bb539bce4f7897be9cdcb55638\' > $RESULTS_DIR/wf_upstream_of_lsp_trend_filePath.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_lsp_trend_filePath.gv > $RESULTS_DIR/wf_upstream_of_lsp_trend_filePath.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_lsp_trend_filePath.gv > $RESULTS_DIR/wf_upstream_of_lsp_trend_filePath.svg
+productName="lsp_trend"
+joinOn="filePath"
+productHashCode=97b9fe2837579bac75e964fe212402ba5ac595bb539bce4f7897be9cdcb55638
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_filePath.sh \'$productHashCode\' > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.svg
 
 # draw worfklow graph upstream of lsp_status.csv using sha256 but display filePath in the results
-$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_filePath.sh \'510e09f1f3ef8fcbc38c43a40e1698cea99ebea15fd64a26339169ba6ce238fc\' > $RESULTS_DIR/wf_upstream_of_lsp_status_filePath.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_lsp_status_filePath.gv > $RESULTS_DIR/wf_upstream_of_lsp_status_filePath.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_lsp_status_filePath.gv > $RESULTS_DIR/wf_upstream_of_lsp_status_filePath.svg
+productName="lsp_status"
+joinOn="filePath"
+productHashCode=510e09f1f3ef8fcbc38c43a40e1698cea99ebea15fd64a26339169ba6ce238fc
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_filePath.sh \'$productHashCode\' > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.svg
 
 # draw worfklow graph upstream of lsp_trend.csv using sha256 but display hash value in the results
-$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_sha256.sh \'97b9fe2837579bac75e964fe212402ba5ac595bb539bce4f7897be9cdcb55638\' > $RESULTS_DIR/wf_upstream_of_lsp_trend_sha256.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_lsp_trend_sha256.gv > $RESULTS_DIR/wf_upstream_of_lsp_trend_sha256.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_lsp_trend_sha256.gv > $RESULTS_DIR/wf_upstream_of_lsp_trend_sha256.svg
+productName="lsp_trend"
+joinOn="sha256"
+productHashCode=97b9fe2837579bac75e964fe212402ba5ac595bb539bce4f7897be9cdcb55638
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_sha256.sh \'$productHashCode\' > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.svg
 
 # draw worfklow graph upstream of lsp_status.csv using sha256 but display hash value in the results
-$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_sha256.sh \'510e09f1f3ef8fcbc38c43a40e1698cea99ebea15fd64a26339169ba6ce238fc\' > $RESULTS_DIR/wf_upstream_of_lsp_status_sha256.gv
-dot -Tpdf $RESULTS_DIR/wf_upstream_of_lsp_status_sha256.gv > $RESULTS_DIR/wf_upstream_of_lsp_status_sha256.pdf
-dot -Tsvg $RESULTS_DIR/wf_upstream_of_lsp_status_sha256.gv > $RESULTS_DIR/wf_upstream_of_lsp_status_sha256.svg
+productName="lsp_status"
+joinOn="sha256"
+productHashCode=510e09f1f3ef8fcbc38c43a40e1698cea99ebea15fd64a26339169ba6ce238fc
+$QUERIES_DIR/render_rm_graph_upstream_of_file_q1_sha256.sh \'$productHashCode\' > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv
+dot -Tpdf $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.pdf
+dot -Tsvg $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.gv > $RESULTS_DIR/wf_upstream_of_$productName'_'$joinOn.svg
